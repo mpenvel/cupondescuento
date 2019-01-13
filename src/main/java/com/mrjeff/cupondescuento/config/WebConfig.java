886c3db238927401464018ea2681620ac8cbbdee
Manuel David Peñaranda Vela <mpenvel@gmail.com>
2019-01-13 20:28:40 +0100

    Añadido Swagger y CORS

---
+++ src/main/java/com/mrjeff/cupondescuento/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.mrjeff.cupondescuento.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PATCH", "PUT", "DELETE").allowCredentials(true);
+	}
+
+}
--- src/main/java/com/mrjeff/cupondescuento/controllers/PedidoController.java
+++ src/main/java/com/mrjeff/cupondescuento/controllers/PedidoController.java
@@ -3,6 +3,8 @@ package com.mrjeff.cupondescuento.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +18,17 @@ import com.mrjeff.cupondescuento.dto.ProductoDTO;
 import com.mrjeff.cupondescuento.exceptions.CuponDescuentoException;
 import com.mrjeff.cupondescuento.services.PedidoService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoController {
 
 	@Autowired
 	private PedidoService pedidoService;
-
+	
 	@PostMapping("/cupon")
+	@ApiOperation(value = "Aplicar código descuento", notes = "Aplica un código de descuento a una lista de productos seleccionados.")
 	public ResponseEntity<Double> aplicarCodigoDescuento(@RequestBody CuponDescuentoDTO cuponDescuentoDTO) throws CuponDescuentoException {
 		String codigoCupon = cuponDescuentoDTO.getCodigoCupon();
 		List<Optional<ProductoDTO>> productosSeleccionados = cuponDescuentoDTO.getProductosSeleccionados();
